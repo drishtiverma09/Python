dbfbd5c107f80c7fc7b03d1ae7809de9659ddfe8
drishtiverma09 <drishti09102@gmail.com>
2026-03-11 09:29:06 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,76 @@
+# Create variables for name, age, and city and print them
+name="Zoe"
+age=19
+city="Chandigarh"
+print(name,age,city)
+
+#Store two numbers in variables and print their sum.
+variable_1=25
+variable_2=50
+sum=variable_1+variable_2
+print("Sum of 2 numbers: ",sum)
+
+#Store a price and quantity and print the total cost.
+price=20
+quantity=15
+total_cost=price*quantity
+print("Total cost:",total_cost)
+
+#Create variables for first name and last name and print the full name.
+first_name="Zoe"
+last_name="Alwats"
+full_name=first_name+last_name
+print(full_name)
+
+# Store a temperature value and print a message with it.
+temperature=25
+print("Temperature is: ",temperature,"degrees")
+
+#Create a variable for your favorite programming language and print it.
+favorite_language="Python"
+print("My favorite language is : ",favorite_language)
+
+#Store your birth year and calculate your age.
+birth_year=2007
+current_year=2026
+current_age=current_year-birth_year
+print("My current age is:",current_age)
+
+# Create variables a, b, c and print their average.
+var_a=18
+var_b=38
+var_c=59
+average=(var_a+var_b+var_c)/3
+print("Average of 3 variables is: ",average)
+
+# Swap two numbers using variables.
+var_1=10
+var_2=30
+var_1,var_2=var_2,var_1
+print("Variable1 and variable 2 respectively after swapping:" ,var_1 ,var_2)
+
+# Store a number and print its square.
+number=3
+square=number*number
+print("Square of the number: ",square)
+
+#  Store a number and print its cube
+number=4
+cube=number*number*number
+print("Cube of number is: ",cube)
+
+# Store a string and print it 3 times. 
+string="This is car"
+print(string*3)
+
+# Create variables for length and width and calculate area. 
+length=12
+width=12
+area_of_Rectangle=length*width
+print("Area of rectangle:",area_of_Rectangle)
+
+# Store a distance and time and calculate speed.
+distance=100
+time=14
+speed=distance*time
+print("Speed of vehicle is:",speed)
